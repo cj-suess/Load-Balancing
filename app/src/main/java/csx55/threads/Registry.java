65e09f76408fa4a27c87a241a428beb5596c00ac
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2025-09-18 08:52:13 -0600

    added basic startRegistry. need to add ComputeNode side to test

--- app/src/main/java/csx55/threads/Registry.java
+++ app/src/main/java/csx55/threads/Registry.java
@@ -1,10 +1,14 @@
 package csx55.threads;
 
-import java.net.Socket;
-
+import csx55.transport.TCPConnection;
 import csx55.util.LogConfig;
 import csx55.wireformats.Event;
+import csx55.wireformats.NodeID;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.*;
+import java.io.IOException;
 import java.net.*;
 
 public class Registry implements Node {
@@ -12,6 +16,9 @@ public class Registry implements Node {
     private Logger log = Logger.getLogger(this.getClass().getName());
     private int port;
     private ServerSocket serverSocket;
+    private boolean running = true;
+
+    private Map<NodeID, TCPConnection> connections = new ConcurrentHashMap<>();
 
     public Registry(int port) {
         this.port = port;
@@ -22,9 +29,28 @@ public class Registry implements Node {
         
     }
 
+    private void startRegistry() {
+        try {
+            serverSocket = new ServerSocket(port);
+            log.info("Registry is up and running. Listening on port: " + port);
+            while(running) {
+                Socket clientSocket = serverSocket.accept();
+                InetSocketAddress client = (InetSocketAddress) clientSocket.getRemoteSocketAddress();
+                log.info("New connection from: " + client.getAddress() + ":" + client.getPort());
+                NodeID nodeID = new NodeID(client.getAddress().getHostAddress(), client.getPort());
+                TCPConnection conn = new TCPConnection(clientSocket, this);
+                conn.startReceiverThread();
+                connections.put(nodeID, conn);
+            }
+        } catch(IOException e) {
+            log.warning("Exception while accepting connections..." + e.getStackTrace());
+        }
+    }
+
     public static void main(String[] args) {
-        
         LogConfig.init(Level.INFO);
-        
+        Registry reg = new Registry(Integer.parseInt(args[0]));
+        new Thread(reg::startRegistry).start();
+        // new Thread(reg::readTerminal).start();
     }
 }
