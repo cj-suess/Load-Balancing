3bee0912b93e55cad3ac710aae76b15c87ef7089
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2025-09-18 08:24:00 -0600

    added overridden equals and hashcode for NodeID object

--- app/src/main/java/csx55/wireformats/NodeID.java
+++ app/src/main/java/csx55/wireformats/NodeID.java
@@ -1,5 +1,7 @@
 package csx55.wireformats;
 
+import java.util.Objects;
+
 public class NodeID {
     
     String ip;
@@ -8,6 +10,19 @@ public class NodeID {
     public NodeID(String ip, int port){
         this.ip = ip;
         this.port = port;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof NodeID)) return false;
+        NodeID nodeID = (NodeID) o;
+        return port == nodeID.port && ip.equals(nodeID.ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip, port);
     }
     
 }
